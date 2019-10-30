b78ba78c7bf3a9fbd17dd83af473d3a9165fd9ad
Blanqui <40902380+blnq11@users.noreply.github.com>
2019-10-29 20:05:33 -0600

    ok

--- green-horizotal/Confi/Conexion.php
+++ green-horizotal/Confi/Conexion.php
@@ -3,7 +3,7 @@ function conectarMysql(){
     $server = "localhost";
     $user = "root";
     $pass = "";
-    $db = "funesi";
+    $db = "funesidb";
 
     $con = mysqli_connect($server,$user,$pass) or die ("Error a Conectar en la BD".mysqli_connect_error());
     mysqli_select_db($con, $db) or die ("Error a Conectar en la Base de datos".mysqli_connect_error());
